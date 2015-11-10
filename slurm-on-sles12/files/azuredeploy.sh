820ae75c3b12392a8744de5d8b603005be553665
Christian Smith <christian.smith@microsoft.com>
2015-11-11 09:48:20 +1100

    Move extra files to subdir update readme.

--- slurm-on-sles12/files/azuredeploy.sh
+++ slurm-on-sles12/files/azuredeploy.sh
@@ -16,46 +16,62 @@ fi
 MASTER_HOSTNAME=$1
 WORKER_HOSTNAME_PREFIX=$2
 WORKER_COUNT=$3
-HPC_USER=$4
 TEMPLATE_BASE_URL="$5"
 LAST_WORKER_INDEX=$(($WORKER_COUNT - 1))
 
-WORKING_DIR="`pwd`"
+# Shares
+SHARE_HOME=/share/home
+SHARE_DATA=/share/data
+
+# Munged
 MUNGE_USER=munge
 MUNGE_GROUP=munge
+MUNGE_VERSION=0.5.11
+
+# SLURM
 SLURM_USER=slurm
 SLURM_UID=6006
 SLURM_GROUP=slurm
 SLURM_GID=6006
 SLURM_VERSION=15-08-1-1
+SLURM_CONF_DIR=$SHARE_DATA/conf
+
+# Hpc User
+HPC_USER=$4
 HPC_UID=7007
 HPC_GROUP=users
-SHARE_HOME=/share/home
-SHARE_DATA=/share/data
 
 
+# Returns 0 if this node is the master node.
+#
 is_master()
 {
     hostname | grep "$MASTER_HOSTNAME"
     return $?
 }
 
+# Add the SLES 12 SDK repository which includes all the
+# packages for compilers and headers.
+#
 add_sdk_repo()
 {
     repoFile="/etc/zypp/repos.d/SMT-http_smt-azure_susecloud_net:SLE-SDK12-Pool.repo"
+	
     if [ -e "$repoFile" ]; then
         echo "SLES 12 SDK Repository already installed"
         return 0
     fi
 	
-	wget $TEMPLATE_BASE_URL/sles12sdk.repo
+	wget $TEMPLATE_BASE_URL/files/sles12sdk.repo
 	
 	cp sles12sdk.repo "$repoFile"
 
-    # init repo
+    # init new repo
     zypper -n search nfs > /dev/null 2>&1
 }
 
+# Installs all required packages.
+#
 install_pkgs()
 {
     pkgs="libbz2-1 libz1 openssl libopenssl-devel gcc gcc-c++ nfs-client rpcbind"
@@ -67,11 +83,15 @@ install_pkgs()
     zypper -n install $pkgs
 }
 
+# Partitions all data disks attached to the VM and creates
+# a RAID-0 volume with them.
+#
 setup_data_disks()
 {
     mountPoint="$1"
 	createdPartitions=""
 
+    # Loop through and partition disks until not found
     for disk in sdc sdd sde sdf sdg sdh sdi sdj sdk sdl sdm sdn sdo sdp sdq sdr; do
         fdisk -l /dev/$disk || break
         fdisk /dev/$disk << EOF
@@ -87,6 +107,7 @@ EOF
         createdPartitions="$createdPartitions /dev/${disk}1"
 	done
 
+    # Create RAID-0 volume
     if [ -n "$createdPartitions" ]; then
         devices=`echo $createdPartitions | wc -w`
         mdadm --create /dev/md10 --level 0 --raid-devices $devices $createdPartitions
@@ -96,6 +117,13 @@ EOF
     fi
 }
 
+# Creates and exports two shares on the master nodes:
+#
+# /share/home (for HPC user)
+# /share/data
+#
+# These shares are mounted on all worker nodes.
+#
 setup_shares()
 {
     mkdir -p $SHARE_HOME
@@ -115,40 +143,38 @@ setup_shares()
     fi
 }
 
+# Downloads/builds/installs munged on the node.  
+# The munge key is generated on the master node and placed 
+# in the data share.  
+# Worker nodes copy the existing key from the data share.
+#
 install_munge()
 {
     groupadd $MUNGE_GROUP
 
     useradd -M -c "Munge service account" -g munge -s /usr/sbin/nologin munge
 
-    mkdir munge
-    cd munge
-
     wget https://github.com/dun/munge/archive/munge-0.5.11.tar.gz
 
-    tar xvfz munge-0.5.11.tar.gz
+    tar xvfz munge-$MUNGE_VERSION.tar.gz
 
-    cd munge-munge-0.5.11*
+    cd munge-munge-$MUNGE_VERSION
 
     mkdir -m 700 /etc/munge
     mkdir -m 711 /var/lib/munge
     mkdir -m 700 /var/log/munge
     mkdir -m 755 /var/run/munge
 
-    ./configure -libdir=/usr/lib64 --prefix=/usr --sysconfdir=/etc --localstatedir=/var
-
-    make
-
-    make install
+    ./configure -libdir=/usr/lib64 --prefix=/usr --sysconfdir=/etc --localstatedir=/var && make && make install
 
     chown -R munge:munge /etc/munge /var/lib/munge /var/log/munge /var/run/munge
 
     if is_master; then
         dd if=/dev/urandom bs=1 count=1024 > /etc/munge/munge.key
-		mkdir -p $SHARE_DATA/slurm
-        cp /etc/munge/munge.key $SHARE_DATA/slurm
+		mkdir -p $SLURM_CONF_DIR
+        cp /etc/munge/munge.key $SLURM_CONF_DIR
     else
-        cp $SHARE_DATA/slurm/munge.key /etc/munge/munge.key
+        cp $SLURM_CONF_DIR/munge.key /etc/munge/munge.key
     fi
 
     chown munge:munge /etc/munge/munge.key
@@ -156,32 +182,37 @@ install_munge()
 
     /etc/init.d/munge start
 
-    cd $WORKING_DIR
+    cd ..
 }
 
+# Installs and configures slurm.conf on the node.
+# This is generated on the master node and placed in the data
+# share.  All nodes create a sym link to the SLURM conf
+# as all SLURM nodes must share a common config file.
+#
 install_slurm_config()
 {
     if is_master; then
-	
-	    wget "$TEMPLATE_BASE_URL/slurm.template.conf"
-		
+
+        mkdir -p $SLURM_CONF_DIR
+
+	    wget "$TEMPLATE_BASE_URL/files/slurm.template.conf"
+
 		cat slurm.template.conf |
 		        sed 's/__MASTER__/'"$MASTER_HOSTNAME"'/g' |
 				sed 's/__WORKER_HOSTNAME_PREFIX__/'"$WORKER_HOSTNAME_PREFIX"'/g' |
-				sed 's/__LAST_WORKER_INDEX__/'"$LAST_WORKER_INDEX"'/g' > slurm.conf
-
-        mkdir -p $SHARE_DATA/slurm
-        mv slurm.conf $SHARE_DATA/slurm/
+				sed 's/__LAST_WORKER_INDEX__/'"$LAST_WORKER_INDEX"'/g' > $SLURM_CONF_DIR/slurm.conf
     fi
 
     ln -s $SHARE_DATA/slurm/slurm.conf /etc/slurm/
 }
 
+# Downloads, builds and installs SLURM on the node.
+# Starts the SLURM control daemon on the master node and
+# the agent on worker nodes.
+#
 install_slurm()
 {
-    mkdir slurm
-    cd slurm
-
     groupadd -g $SLURM_GID $SLURM_GROUP
 
     useradd -M -u $SLURM_UID -c "SLURM service account" -g $SLURM_GROUP -s /usr/sbin/nologin $SLURM_USER
@@ -195,9 +226,8 @@ install_slurm()
     tar xvfz slurm-$SLURM_VERSION.tar.gz
 
     cd slurm-slurm-$SLURM_VERSION
-    ./configure -libdir=/usr/lib64 --prefix=/usr --sysconfdir=/etc/slurm && make
-    make
-    make install
+	
+    ./configure -libdir=/usr/lib64 --prefix=/usr --sysconfdir=/etc/slurm && make && make install
 
     install_slurm_config
 
@@ -207,16 +237,22 @@ install_slurm()
         /usr/sbin/slurmd -vvvv
     fi
 
-    cd $WORKING_DIR
+    cd ..
 }
 
+# Adds a common HPC user to the node and configures public key SSh auth.
+# The HPC user has a shared home directory (NFS share on master) and access
+# to the data share.
+#
 setup_hpc_user()
 {
     if is_master; then
         useradd -c "HPC User" -g $HPC_GROUP -d $SHARE_HOME/$HPC_USER -s /bin/bash -m -u $HPC_UID $HPC_USER
-        sudo -u $HPC_USER ssh-keygen -t rsa -f $SHARE_HOME/$HPC_USER/.ssh/id_rsa -q -P ""
 
+        # Configure public key auth for the HPC user
+        sudo -u $HPC_USER ssh-keygen -t rsa -f $SHARE_HOME/$HPC_USER/.ssh/id_rsa -q -P ""
         cat $SHARE_HOME/$HPC_USER/.ssh/id_rsa.pub > $SHARE_HOME/$HPC_USER/.ssh/authorized_keys
+
         echo "Host *" > $SHARE_HOME/$HPC_USER/.ssh/config
         echo "    StrictHostKeyChecking no" >> $SHARE_HOME/$HPC_USER/.ssh/config
         echo "    UserKnownHostsFile /dev/null" >> $SHARE_HOME/$HPC_USER/.ssh/config
@@ -229,9 +265,12 @@ setup_hpc_user()
         useradd -c "HPC User" -g $HPC_GROUP -d $SHARE_HOME/$HPC_USER -s /bin/bash -u $HPC_UID $HPC_USER
     fi
 
+    # Don't require password for HPC user sudo
     echo "$HPC_USER ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 }
 
+# Sets all common environment variables and system parameters.
+#
 setup_env()
 {
     # Set unlimited mem lock
