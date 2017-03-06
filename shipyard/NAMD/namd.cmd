97bc6984eaa3bb35c2ca64295d5dfe020bc10758
Christian Smith <smith.cba@gmail.com>
2017-03-06 14:50:11 +1100

    Init script to install shipyard Pool templating

--- shipyard/NAMD/namd.cmd
+++ shipyard/NAMD/namd.cmd
@@ -1,17 +1,38 @@
 @echo off
 
-rem set recipe=NAMD-TCP
-set recipe=NAMD-Infiniband-IntelMPI
+rem You can update the recipe to 'NAMD-Infiniband-IntelMPI' for MPI jobs
+set recipe=NAMD-TCP
 
 set script_dir=%~dp0.
-set shipyard_dir=%script_dir%\shipyard
-set shipyard=%shipyard_dir%\shipyard.py
-set python_dir=%shipyard_dir%\Python35
-set python=%python_dir%\python.exe
-set config_dir=%shipyard_dir%\recipes\%recipe%\config
 set powershell=C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe
+set python=python.exe
+
+rem Check for Python in path
+cmd /c where %python% > nul 2>&1
+if %errorlevel% neq 0 (
+    rem Check for Python in PYTHONPATH
+    set python=%PYTHONPATH%\python.exe
+    cmd /c where %python% > nul 2>&1
+    if %errorlevel% neq 0 (
+        echo Please install python 3.5+
+        exit /b 1
+    )
+)
+
+cmd /c where pip3.exe > nul 2>&1
+if %errorlevel% neq 0 (
+    echo Please install pip
+    exit /b 1
+)
 
 cmd /c where blobxfer.exe > nul 2>&1
-if %errorlevel% neq 0 set PATH="%python_dir%;%python_dir%\Scripts;%PATH%"
+if %errorlevel% neq 0 (
+    echo Please install blobxfer
+    exit /b 1
+)
 
-%powershell% -exec bypass -file %script_dir%\shipyard.ps1 -namdConfFilePath %1 -namdArgs "%2" -recipe %recipe%
+if "%2" neq "" (
+
+)
+
+%powershell% -exec bypass -file %script_dir%\namd.ps1 -namdConfFilePath %1 -namdArgs "%2" -recipe %recipe% -poolId namd-tcp
--- shipyard/NAMD/namd.ps1
+++ shipyard/NAMD/namd.ps1
@@ -1,8 +1,9 @@
-
 Param(
-  [string]$namdConfFilePath = "mdff-domain\\domain-step1.namd",
+  [string]$namdConfFilePath,
   [string]$namdArgs = "",
-  [string]$recipe = "NAMD-TCP"
+  [string]$recipe = "NAMD-TCP",
+  [string]$poolId = $null,
+  [switch]$poolKeepAlive
 )
 
 if (-Not (Test-Path $namdConfFilePath))
@@ -11,11 +12,25 @@ if (-Not (Test-Path $namdConfFilePath))
     exit 1
 }
 
-$jobId = "namd-" + [guid]::NewGuid().ToString()
+$id = [guid]::NewGuid().ToString()
+$jobId = "namd-$id"
 $rootDir = "$PSScriptRoot"
+$shipyard = "$rootDir\\shipyard\\shipyard.py"
 $tmpDir = "$rootDir\\.shipyard_tmp\\$jobId"
 $tmpInputsDir = "$tmpDir\\inputs"
-$shipyardDir = "$rootDir\\shipyard\\recipes\\$recipe"
+$shipyardDir = "$rootDir\\shipyard-recipes\\$recipe"
+
+if ($poolId)
+{
+    # Pool spcified, no auto pool
+    $autoPool = $false
+}
+else
+{
+    # No pool, lets create and delete one for the job
+    $poolId = $id
+    $autoPool = $true
+}
 
 $namdConfFile = Split-Path $namdConfFilePath -leaf
 $namdInputDir = (get-item $namdConfFilePath).Directory.FullName
@@ -26,10 +41,10 @@ mkdir "$tmpInputsDir" | out-null
 Copy-Item -Path "$namdInputDir\\*" -Destination "$tmpInputsDir" -Recurse
 
 # Copy the helper scripts
-Copy-Item -Path "$rootDir\\Scripts\\*" -Destination "$tmpInputsDir" -Recurse
+Copy-Item -Path "$rootDir\\scripts\\*" -Destination "$tmpInputsDir" -Recurse
 
 # Copy the shipyard configs and replace and variables needed
-$shipyardFilesToCopy = "$shipyardDir\\config\\credentials.json","$shipyardDir\\config\\config.json","$shipyardDir\\config\\jobs.json","$shipyardDir\\config\\pool.json"
+$shipyardFilesToCopy = "$rootDir\\credentials.json","$shipyardDir\\config.json","$shipyardDir\\jobs.json","$shipyardDir\\pool.json"
 foreach ($file in $shipyardFilesToCopy)
 {
     $filename = Split-Path $file -leaf
@@ -37,13 +52,14 @@ foreach ($file in $shipyardFilesToCopy)
     $sourcePath = $tmpInputsDir -replace "\\", "/"
     get-content $file | foreach-object {
 		$_ -replace "@JOB_ID@", "$jobId" `
+           -replace "@POOL_ID@", "$poolId" `
 		   -replace "@SOURCE_PATH@", "$sourcePath" `
 		   -replace "@NAMD_INPUT_FILE@", "$namdConfFile" `
            -replace "@NAMD_ARGS@", "$namdArgs" } | set-content $destination
 }
 
 Write-Host "Uploading job inputs..."
-$output = & $env:python $env:shipyard data ingress --configdir "$tmpDir" 2>&1
+$output = & $env:python $shipyard data ingress --configdir "$tmpDir" 2>&1
 if ($lastexitcode -ne 0)
 {
     Write-Host "Failed to upload input data"
@@ -51,8 +67,19 @@ if ($lastexitcode -ne 0)
     exit 1
 }
 
+if ($autoPool)
+{
+    Write-Host "Creating pool..."
+    & $env:python $shipyard pool add --configdir "$tmpDir" --yes
+    if ($lastexitcode -ne 0)
+    {
+        Write-Host "Failed to create pool"
+        exit 1
+    }
+}
+
 Write-Host "Submitting job $jobId..."
-& $env:python $env:shipyard jobs add --configdir "$tmpDir" --yes --tail stdout.txt
+& $env:python $shipyard jobs add --configdir "$tmpDir" --yes --tail stdout.txt
 if ($lastexitcode -ne 0)
 {
     Write-Host "Failed to submit job"
@@ -60,19 +87,30 @@ if ($lastexitcode -ne 0)
 }
 
 Write-Host "Downloading job outputs..."
-$output = & $env:python $env:shipyard data getfile --configdir "$tmpDir" --all --filespec "$jobId,dockertask-00000,std*.txt" 2>&1
+$output = & $env:python $shipyard data getfile --configdir "$tmpDir" --all --filespec "$jobId,dockertask-00000,std*.txt" 2>&1
 if ($lastexitcode -ne 0)
 {
 	Write-Host $output
     Write-Host "Failed to retrieve outputs"
 }
 
-$output = & $env:python $env:shipyard data getfile --configdir "$tmpDir" --all --filespec "$jobId,dockertask-00000,wd/*" 2>&1
+$output = & $env:python $shipyard data getfile --configdir "$tmpDir" --all --filespec "$jobId,dockertask-00000,wd/*" 2>&1
 if ($lastexitcode -ne 0)
 {
     Write-Host $output
 	Write-Host "Failed to retrieve outputs"
     exit 1
+}
+
+if ($autoPool -a $poolKeepAlive -eq $false)
+{
+    Write-Host "Deleting pool..."
+    & $env:python $shipyard pool del --configdir "$tmpDir" --yes
+    if ($lastexitcode -ne 0)
+    {
+        Write-Host "Failed to delete pool"
+        exit 1
+    }
 }
 
 Remove-Item $tmpInputsDir -Force -Recurse | out-null
